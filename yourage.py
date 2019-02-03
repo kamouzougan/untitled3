8e9662c761db055065c2a218c8679dc37f656374
Kangni <kamouzougan@gmail.com>
2019-02-03 13:09:17 -0500

    Initial commit

---
+++ yourage.py
@@ -0,0 +1,5 @@
+name = input ("Please enter your name :")
+age = int (input ("please enter your age :"))
+total = str ((2019 -age) + 100)
+
+print("Welecome  " + name+ " you will be 100 years old  in " + total)
